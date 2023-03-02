500475c3cfd9b0cb2cdcd7133585b48752acbac4
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-03-03 02:34:09 +0300

    Topbar Component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,45 @@
 import './Topbar.css';
+import { Search, Person, Chat, Notifications } from '@mui/icons-material';
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar-container">
+      <div className="topbar-left">
+        <span className="logo">SOCIAL</span>
+      </div>
+      <div className="topbar-center">
+        <div className="search-bar">
+          <Search className="search-icon" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for friend, post, or video"
+          />
+        </div>
+      </div>
+      <div className="topbar-right">
+        <div className="topbar-links">
+          <span className="topbar-link">Home</span>
+          <span className="topbar-link">Timeline</span>
+        </div>
+        <div className="topbar-icons">
+          <div className="topbar-icon-item">
+            <Person />
+            <span className="topbar-icon-badge">1</span>
+          </div>
+          <div className="topbar-icon-item">
+            <Chat />
+            <span className="topbar-icon-badge">2</span>
+          </div>
+          <div className="topbar-icon-item">
+            <Notifications />
+            <span className="topbar-icon-badge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbar-image" />
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
