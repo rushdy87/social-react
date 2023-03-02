61bef168fad3ff723dc1bee7d38228bf07d11e67
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-03-03 00:50:07 +0300

    App installations

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,7 @@
+import './Topbar.css';
+
+const Topbar = () => {
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import { Topbar } from '../../components';
+import './Home.css';
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default Home;
